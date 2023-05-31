187ae6417188c5f01b02df7662d5fd02c0bce159
Realkayzee <olaniyikazeem112@gmail.com>
2023-05-31 17:39:49 +0100

    frontend interaction setup

--- packages/react-app/pages/_app.tsx
+++ packages/react-app/pages/_app.tsx
@@ -2,12 +2,14 @@ import type { AppProps } from "next/app";
 import { RainbowKitProvider, connectorsForWallets } from "@rainbow-me/rainbowkit";
 import { configureChains, createConfig, WagmiConfig } from "wagmi";
 import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
-import celoGroups from "@celo/rainbowkit-celo/lists";
 import { Alfajores, Celo } from "@celo/rainbowkit-celo/chains";
 import Layout from "../components/Layout";
 import "../styles/globals.css";
 import "@rainbow-me/rainbowkit/styles.css";
 import 'react-toastify/dist/ReactToastify.css';
+import {metaMaskWallet, omniWallet, rainbowWallet, walletConnectWallet} from "@rainbow-me/rainbowkit/wallets"
+import {ToastContainer} from "react-toastify";
+import { Valora, CeloWallet } from "@celo/rainbowkit-celo/wallets";
 
 const projectId = "celo-composer-project-id" // get one at https://cloud.walletconnect.com/app
 
@@ -15,8 +17,22 @@ const { chains, publicClient } = configureChains(
   [Alfajores, Celo],
   [jsonRpcProvider({ rpc: (chain) => ({ http: chain.rpcUrls.default.http[0] }) })]
 );
+
+// Create list of wallet to connect to
+const connectors = connectorsForWallets([
+  {
+    groupName: "Recommended with Celo",
+    wallets: [
+      Valora({projectId, chains}),
+      CeloWallet({projectId, chains}),
+      metaMaskWallet({projectId, chains}),
+      rainbowWallet({projectId, chains}),
+      omniWallet({chains}),
+      walletConnectWallet({chains})
+    ]
+  }
   
-const connectors = celoGroups({chains, projectId, appName: typeof document === "object" && document.title || "Your App Name"})
+])
 
 const wagmiConfig = createConfig({
   autoConnect: true,
@@ -28,6 +44,7 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <WagmiConfig config={wagmiConfig}>
       <RainbowKitProvider chains={chains} coolMode={true}>
+        <ToastContainer position="bottom-center" />
         <Layout>
           <Component {...pageProps} />
         </Layout>
