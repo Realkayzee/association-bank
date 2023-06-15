3a5098d39dc3459788645a093bbbb5384abb9127
Realkayzee <olaniyikazeem112@gmail.com>
2023-06-15 23:33:31 +0100

    .

--- packages/react-app/tailwind.config.js
+++ packages/react-app/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "../../node_modules/flowbite-react/**/*.js",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,27 +9,21 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        /** primary */
-        "prosperity": "#FCFF52",
-        "forest": "#476520",
-        /** base */
-        "gypsum": "#FCF6F1",
-        "sand": "#E7E3D4",
-        "wood": "#655947",
-        "fig": "#1E002B",
-        /** functional */
-        "snow": "#FFFFFF",
-        "onyx": "#000000",
-        "success": "#329F3B",
-        "error": "#E70532",
-        "disabled": "#9B9B9B",
-        /** accent */
-        "sky": "#7CC0FF",
-        "citrus": "#FF9A51",
-        "lotus": "#FFA3EB",
-        "lavender": "#B490FF"
+        // footer background
+        "gray15": "#262626",
+        // primary
+        "goldenyellow": "#eeba2c",
+        // modal overlay
+        "neutral-0.5": "#000000b8"
+      },
+      fontFamily: {
+        fira: ['var(--font-fira)'],
+        fira_mono: ['var(--font-fira-mono'],
+        lato: ['var(--font-lato)']
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require("flowbite/plugin")
+  ]
 }
