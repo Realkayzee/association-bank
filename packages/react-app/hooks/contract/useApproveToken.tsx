270dc01f0b16a706b5fdf58c783687e55d485265
Realkayzee <olaniyikazeem112@gmail.com>
2023-06-17 03:24:57 +0100

    updated approve hook to be flexible

--- packages/react-app/hooks/contract/useApproveToken.tsx
+++ packages/react-app/hooks/contract/useApproveToken.tsx
@@ -2,9 +2,11 @@
 // import wagmi hook to prepare and write to a smart contract
 
 import { useContractWrite, usePrepareContractWrite, useWaitForTransaction, useContractRead } from "wagmi";
-import tokenABI from "@/abi/erc20ABI.json"
+import tokenABI from "@/abi/erc20ABI.json";
+import associationAbi from "@/abi/association.abi.json";
 import { assBankCA } from "./useContractSend";
 import { ethers } from "ethers";
+import { HexToDecimal } from "@/components/helpers";
 
 
 // Contract address of cUSDA
@@ -13,11 +15,16 @@ export const cUSDCA = "0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1";
 
 interface approveParamsProps {
     price: string | number;
+    user?: string;
+    receipient?: string;
+    functionName: string;
+    args: Array<any>;
 }
 
-export const useApproveToken = ({price}: approveParamsProps) => {
+export const useApproveToken = ({price, user, receipient, functionName, args}: approveParamsProps) => {
 
-    // Prepare write to smart contract
+
+    // Prepare write to smart contract for token contract
     
     const {config} = usePrepareContractWrite({
         address: cUSDCA,            // The address of the smart contract,
@@ -30,7 +37,7 @@ export const useApproveToken = ({price}: approveParamsProps) => {
     })
 
 
-    // write to the smart contract using the prepared config
+    // write to the smart contract using the prepared config for token contract
 
     const {data:writeData, isLoading:approveTokenLoading, write:tokenWrite } = useContractWrite(config);
 
@@ -38,5 +45,44 @@ export const useApproveToken = ({price}: approveParamsProps) => {
         hash: writeData?.hash
     })
 
-    return { tokenWrite, approveTokenLoading, approveError, approveSuccess, approveLoading }
+    // prepare contract for contract call
+
+    const {config:contractConfig} = usePrepareContractWrite({
+        address: assBankCA,
+        abi: associationAbi,
+        functionName,
+        args
+    })
+
+    const {data:contractData, isLoading:writeLoading, write} = useContractWrite(contractConfig)
+
+    const {isError:waitError, isSuccess:waitSuccess, isLoading:waitLoading} = useWaitForTransaction({
+        hash: contractData?.hash
+    })
+
+    const { data:tokenRead } = useContractRead({
+        address: cUSDCA,
+        abi: tokenABI,
+        functionName: "allowance",
+        args: [
+            user,
+            receipient
+        ]
+    })
+
+    /* This function checks if association bank contract has the necessary amount
+    ** allowance before requesting for amount approval
+    */
+
+    const tokenAuthorization = () => {
+        const priceinput = ethers.utils.parseEther(price ? price.toString() : "0")
+        // @ts-ignore
+        if(HexToDecimal(tokenRead?._hex) > HexToDecimal(priceinput?._hex)) {
+            write?.()
+        }else {
+            tokenWrite?.()
+        }
+    }
+
+    return { approveTokenLoading, approveError, approveSuccess, approveLoading, tokenAuthorization, writeLoading, waitError, waitSuccess, waitLoading }
 }
