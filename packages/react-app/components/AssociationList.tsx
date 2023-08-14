210eb1e859a5aff5d019e4d84967e9b4fd37a6cc
Realkayzee <olaniyikazeem112@gmail.com>
2023-08-15 00:51:48 +0100

    fix minor bug

--- packages/react-app/components/AssociationList.tsx
+++ packages/react-app/components/AssociationList.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Table } from "flowbite-react";
 import { customTheme } from "./customTheme";
 import Link from "next/link";
 import { useContractCall } from "@/hooks/contract/useContractCall";
@@ -24,48 +23,48 @@ const AssociationList = () => {
     ]
     
     return (
-        <div>
-            <Table>
-                <Table.Head className="bg-black">
+        <table className="table-auto w-full">
+            <thead className="bg-black">
+                <tr>
                     {
                         tableHeadCell.map((tableHeadCell, id) => (
-                            <Table.HeadCell key={id} className="bg-yellow-400 font-bold text-md">
+                            <th key={id} className="bg-goldenyellow text-black py-3 font-bold text-md">
                                 {tableHeadCell}
-                            </Table.HeadCell>
+                            </th>
                         ))
                     }
-                </Table.Head>
-                <Table.Body className="divide-y divide-neutral-700 text-neutral-400 bg-black">
-                    {
-                        associationData?.map((details:any, id:any) => (
-                            <Table.Row key={id} className="hover:bg-black">
-                                <Table.Cell>
-                                    {details.infoAssName}
-                                </Table.Cell>
-                                <Table.Cell>
-                                    {Number(details.associationAcctNumber).toString().padStart(4, "0")}
-                                </Table.Cell>
-                                <Table.Cell>
-                                    {(details.infoAssCreator).slice(0, 6)}....{(details.infoAssCreator).slice(35, 42)}
-                                </Table.Cell>
-                                <Table.Cell>
-                                    <Link href={`/dashboard/${id + 1}`}>
-                                        <button
-                                            type="button"
-                                            className={`${customTheme.outline_button}`}
-                                        >
-                                            <span className={`${customTheme.button_span} bg-black`}>
-                                                View more
-                                            </span>
-                                        </button>
-                                    </Link>
-                                </Table.Cell>
-                            </Table.Row>
-                        ))
-                    }
-                </Table.Body>
-            </Table>
-        </div>
+                </tr>
+            </thead>
+            <tbody className="divide-y divide-neutral-700 text-neutral-400 bg-black">
+                {
+                    associationData?.map((details:any, id:any) => (
+                        <tr key={id} className="hover:bg-black">
+                            <th className="py-10">
+                                {details.infoAssName}
+                            </th>
+                            <th>
+                                {Number(details.associationAcctNumber).toString().padStart(4, "0")}
+                            </th>
+                            <th>
+                                {(details.infoAssCreator).slice(0, 6)}....{(details.infoAssCreator).slice(35, 42)}
+                            </th>
+                            <th>
+                                <Link href={`/dashboard/${id + 1}`}>
+                                    <button
+                                        type="button"
+                                        className={`${customTheme.outline_button}`}
+                                    >
+                                        <span className={`${customTheme.button_span} bg-black`}>
+                                            View more
+                                        </span>
+                                    </button>
+                                </Link>
+                            </th>
+                        </tr>
+                    ))
+                }
+            </tbody>
+        </table>
     );
 }
 
